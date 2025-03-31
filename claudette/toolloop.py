661451e23479c424b2d6fd0e2d74bd4dc8603d1a
Chris Kroenke <christopher.kroenke@gmail.com>
2025-03-31 18:15:55 -0400

    add prefill to first call in toolloop

--- claudette/toolloop.py
+++ claudette/toolloop.py
@@ -11,6 +11,7 @@ from fastcore.meta import delegates
 from anthropic.types import TextBlock, Message, ToolUseBlock
 
 # %% ../01_toolloop.ipynb
+#| exports
 @patch
 @delegates(Chat.__call__)
 def toolloop(self:Chat,
@@ -18,10 +19,20 @@ def toolloop(self:Chat,
              max_steps=10, # Maximum number of tool requests to loop through
              trace_func:Optional[callable]=None, # Function to trace tool use steps (e.g `print`)
              cont_func:Optional[callable]=noop, # Function that stops loop if returns False
+             first_call_prefill:str="", # Prefill for the first call
              **kwargs):
     "Add prompt `pr` to dialog and get a response from Claude, automatically following up with `tool_use` messages"
     n_msgs = len(self.h)
-    r = self(pr, **kwargs)
+
+    # add optional prefill tool steering for the first call
+    if first_call_prefill != "":
+        first_kwargs = kwargs.copy()
+        first_kwargs['prefill'] = first_call_prefill
+    else:
+        first_kwargs = kwargs
+
+    r = self(pr, **first_kwargs)
+
     for i in range(max_steps):
         if r.stop_reason!='tool_use': break
         if trace_func: trace_func(self.h[n_msgs:]); n_msgs = len(self.h)
